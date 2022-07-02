59d441c0bae9fc9099d740b6c40ff9d4b6c752bd
Gregor Vilkner <gregor.vilkner@thinkiq.com>
2022-07-02 11:26:47 -0400

    remove bootstrap imports

--- vue-desktop/src/main.js
+++ vue-desktop/src/main.js
@@ -1,13 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// import BootstrapVue from "bootstrap-vue"
-// import "bootstrap/dist/css/bootstrap.min.css"
-// import "bootstrap-vue/dist/bootstrap-vue.css"
-
-
 // createApp(App).mount('#app')
 
 const app = createApp(App)
-// app.use(BootstrapVue)
 app.mount('#app')
